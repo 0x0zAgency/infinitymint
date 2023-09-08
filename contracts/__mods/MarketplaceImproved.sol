d4e467de6a1a740eb81cc3c4148cb0a7012ef9ec
0xWizardof0x <z@0x0z.me>
2023-09-08 13:05:42 -0500

    Removing old _mods folder and adding _mods to ignore

--- contracts/__mods/MarketplaceImproved.sol
+++
@@ -1,9 +0,0 @@
-//SPDX-License-Identifier: UNLICENSED
-//0xTinman.eth 2021
-pragma solidity ^0.8.0;
-
-import 'infinitymint/alpha/InfinityMint.sol';
-import 'infinitymint/alpha/InfinityMintApi.sol';
-import 'infinitymint/alpha/IERC721.sol';
-
-contract Mod_MarketplaceImproved is InfinityMintObject {}
